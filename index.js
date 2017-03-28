12791a49590559f13549b75d5d48e2508013cb67
hmauderly <hugues.mauderly@webedia-group.com>
2017-03-28 12:10:20 +0200

    test redis

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 
 const bodyParser = require('body-parser');
 const config = require('config');
+const redis = require('redis');
 const express = require('express');
 const http = require('http');
 const request = require('request');
 
-
+const redisClient = redis.createClient({host : 'localhost', port : 6379});
 
 var app = express();
 var router = express.Router();
@@ -15,12 +16,24 @@ var router = express.Router();
 app.set('port', process.env.PORT || 5555);
 app.use(bodyParser.json());
 
+redisClient.on('ready',function() {
+    console.log("Redis is ready");
+});
+
+redisClient.on('error',function() {
+    console.log("Error in Redis");
+});
+
+redisClient.set("test","blabla");
+
 
 router.get('/test/webhook', function(req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === config.get('test.clientAccessToken')) {
         console.log("Validating webhook");
+        redisClient.set("test","ok");
         res.status(200).send(req.query['hub.challenge'] + '/' + config.get('test.serverResponse'));
+
     } else {
         console.error("Failed validation. Make sure the validation tokens match.");
         res.sendStatus(403);
